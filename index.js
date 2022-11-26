26235faf7a518f0bba243d37f5f6640f80ffb552
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-25 21:59:29 -0600

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+  const userinfo = {
+    name,
+    email,
+  }
+ return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userinfo),
+  })
+  .then(resp => resp.json())
+  .then((data) => document.body.innerHTML = data.id)
+  .catch(error => document.body.innerHTML = error.message)
+}
+
+
+  // fetch('http://localhost:3000/users', {
+  // method: "POST",
+  // headers: {
+  //   "Content-Type": "application/json",
+  //   Accept: "application/json",
+  // },
+  // body: JSON.stringify({
+  //   userName: "Name",
+  //   userEmail: "Email",
+  // })
+  // return fetch()
